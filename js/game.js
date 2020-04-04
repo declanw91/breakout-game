678b0764a83f3d8c4819b28e07d04326f2fdadb6
Declan <dwright1991@gmail.com>
2020-04-04 20:21:48 +0100

    updates to make game paddle work

--- js/game.js
+++ js/game.js
@@ -6,3 +6,18 @@ let gameHeight = canvas.height;
 
 let playerPaddle = new PlayerPaddle(gameWidth, gameHeight);
 playerPaddle.draw(context);
+
+let inputHandler = new InputHandler(playerPaddle);
+
+let lastTime = 0;
+
+function GameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  playerPaddle.update(deltaTime);
+  playerPaddle.draw(context);
+  requestAnimationFrame(GameLoop);
+}
+
+GameLoop();
